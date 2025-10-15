14f6dfe5ab54da0498a3f235e6f50530b50053f8
Jean Carlo <jeancarlomolta@gmail.com>
2025-10-15 10:35:53 -0500

    refractor: hero section

--- Komorebi/src/components/HeroSection.tsx
+++ Komorebi/src/components/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection: React.FC = () => {
         <p className="text-lg mb-10 text-[var(--komorebi-black)]">
           From artisanal onigiri to international treats, find and sell the most delicious snacks in our curated marketplace. Connect with food lovers and discover your next favorite bite.
         </p>
-        <button className="bg-[var(--komorebi-yellow)] text-[var(--komorebi-black)] px-6 py-3 rounded-full hover:bg-[var(--komorebi-gray)] transition-colors">
+        <button className="bg-[var(--komorebi-yellow)] text-[var(--komorebi-black)] px-6 py-3 rounded-full hover:bg-[var(--komorebi-gray)] transition-colors font-bold">
           Shop Now
         </button>
       </div>
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Komorebi-E-commerce
-aye
+aye commits:
+
+feat: new feature (example: feat(product): add filters)
+- fix: bug fix
+- docs: documentation only
+- style: formatting changes without affecting functionality
+- refactor: code improvement without new feature or bug fix
+- file: changes in scaffolding or new files

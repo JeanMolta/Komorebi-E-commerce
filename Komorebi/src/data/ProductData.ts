fe1eb013b6d8f0b003a1123a50e5c7febbf2da7e
MatthwX <mathewalexander2023@gmail.com>
2025-10-17 10:49:03 -0500

    style: product card improvement

--- Komorebi/src/data/ProductData.ts
+++ Komorebi/src/data/ProductData.ts
@@ -1,24 +1,141 @@
 import type { Product } from './ProductTypes';
 
 export const products: Product[] = [
-  { "id": "prod_001", "name": "Onigiri de Salmón", "vendor": "Sakura Onigiri Co.", "price": 15000 },
-  { "id": "prod_002", "name": "Onigiri de Atún Picante", "vendor": "Sakura Onigiri Co.", "price": 16000 },
-  { "id": "prod_003", "name": "Onigiri de Umeboshi (Ciruela ácida japonesa)", "vendor": "ZenRice Snacks", "price": 14500 },
-  { "id": "prod_004", "name": "Mochi de Té Verde", "vendor": "Mochi Mágico", "price": 8000 },
-  { "id": "prod_005", "name": "Pocky Chocolate", "vendor": "Glico International", "price": 7000 },
-  { "id": "prod_006", "name": "KitKat Té Matcha", "vendor": "Nestlé Japón", "price": 9000 },
-  { "id": "prod_007", "name": "Dorayaki con Frijol Rojo", "vendor": "Dora Snacks", "price": 11000 },
-  { "id": "prod_008", "name": "Chocolate Hershey’s Classic", "vendor": "Hershey’s Colombia", "price": 6000 },
-  { "id": "prod_009", "name": "Twix Caramelo", "vendor": "Mars International", "price": 6500 },
-  { "id": "prod_010", "name": "KitKat Negro", "vendor": "Nestlé Internacional", "price": 8800 },
-  { "id": "prod_011", "name": "Haribo Ositos", "vendor": "Haribo Global", "price": 7500 },
-  { "id": "prod_012", "name": "Lays Papas Clásicas", "vendor": "PepsiCo Snacks", "price": 6400 },
-  { "id": "prod_013", "name": "Cheetos Flamin’ Hot", "vendor": "PepsiCo Snacks", "price": 7000 },
-  { "id": "prod_014", "name": "Chocoramo Mini", "vendor": "Productos Ramo", "price": 4200 },
-  { "id": "prod_015", "name": "Takis Fuego", "vendor": "Barcel Internacional", "price": 9500 },
-  { "id": "prod_016", "name": "Biscoff Cookies", "vendor": "Lotus Biscoff", "price": 13000 },
-  { "id": "prod_017", "name": "Kinder Bueno", "vendor": "Ferrero", "price": 12000 },
-  { "id": "prod_018", "name": "Twix Blanco", "vendor": "Mars Internacional", "price": 9000 },
-  { "id": "prod_019", "name": "Ramen Instantáneo Premium", "vendor": "Nissin Japonés", "price": 10500 },
-  { "id": "prod_020", "name": "Galletas Oreo Doble Crema", "vendor": "Mondelez", "price": 6800 }
+  { id: "prod_001", 
+    imageUrl: "https://www.eatwithcarmen.com/wp-content/uploads/2024/02/salmon-onigiri-recipe.jpg",
+    name: "Onigiri de Salmón", 
+    vendor: "Sakura Onigiri Co.", 
+    price: 15000 
+  },
+
+  { id: "prod_002", 
+    imageUrl: "https://www.blogexquisit.es/wp-content/uploads/onigiri-con-atun.jpg",
+    name: "Onigiri de Atún Picante", 
+    vendor: "Sakura Onigiri Co.", 
+    price: 16000 
+  },
+
+  { id: "prod_003", 
+    imageUrl: "https://chefjacooks.com/wp-content/uploads/2022/09/ume-onigiri-square.jpg",
+    name: "Onigiri de Umeboshi (Ciruela ácida japonesa)", 
+    vendor: "ZenRice Snacks", 
+    price: 14500 
+  },
+
+  { id: "prod_004", 
+    imageUrl: "WA",
+    name: "Mochi de Té Verde", 
+    vendor: "Mochi Mágico", 
+    price: 8000 
+  },
+
+  { id: "prod_005", 
+    imageUrl: "W",
+    name: "Pocky Chocolate", 
+    vendor: "Glico International", 
+    price: 7000 
+  },
+
+  { id: "prod_006", 
+    imageUrl: "A",
+    name: "KitKat Té Matcha", 
+    vendor: "Nestlé Japón", 
+    price: 9000 
+  },
+
+  { id: "prod_007", 
+    imageUrl: "wa",
+    name: "Dorayaki con Frijol Rojo", 
+    vendor: "Dora Snacks", 
+    price: 11000 
+  },
+
+  { id: "prod_008", 
+    imageUrl: "wi",
+    name: "Chocolate Hershey’s Classic", 
+    vendor: "Hershey’s Colombia", 
+    price: 6000 
+  },
+  
+  { id: "prod_009", 
+    imageUrl: "wa",
+    name: "Twix Caramelo", 
+    vendor: "Mars International", 
+    price: 6500 },
+
+  { id: "prod_010", 
+    imageUrl: "mi",
+    name: "KitKat Negro", 
+    vendor: "Nestlé Internacional", 
+    price: 8800 
+  },
+
+  { id: "prod_011", 
+    imageUrl: "iwjda",
+    name: "Haribo Ositos", 
+    vendor: "Haribo Global", 
+    price: 7500 
+  },
+
+  { id: "prod_012", 
+    imageUrl: "wa",
+    name: "Lays Papas Clásicas", 
+    vendor: "PepsiCo Snacks", 
+    price: 6400 
+  },
+
+  { id: "prod_013", 
+    imageUrl: "wa",
+    name: "Cheetos Flamin’ Hot", 
+    vendor: "PepsiCo Snacks", 
+    price: 7000 
+  },
+
+  { id: "prod_014", 
+    imageUrl: "a",
+    name: "Chocoramo Mini", 
+    vendor: "Productos Ramo", 
+    price: 4200 
+  },
+
+  { id: "prod_015", 
+    imageUrl: "sa",
+    name: "Takis Fuego", 
+    vendor: "Barcel Internacional", 
+    price: 9500 
+  },
+
+  { id: "prod_016", 
+    imageUrl: "asd",
+    name: "Biscoff Cookies", 
+    vendor: "Lotus Biscoff", 
+    price: 13000 
+  },
+
+  { id: "prod_017", 
+    imageUrl: "wad",
+    name: "Kinder Bueno", 
+    vendor: "Ferrero", 
+    price: 12000 
+  },
+
+  { id: "prod_018", 
+    imageUrl: "ad",
+    name: "Twix Blanco", 
+    vendor: "Mars Internacional", 
+    price: 9000 
+  },
+
+  { id: "prod_019", 
+    imageUrl: "af",
+    name: "Ramen Instantáneo Premium", 
+    vendor: "Nissin Japonés", 
+    price: 10500 
+  },
+
+  { id: "prod_020", 
+    imageUrl: "ads",
+    name: "Galletas Oreo Doble Crema", 
+    vendor: "Mondelez", 
+    price: 6800 }
 ];
